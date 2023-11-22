ad39ba56f43dcf2e311cccd35e1230f369ccf339
EdSpurrier <ed@edspurrier.com>
2023-11-22 21:23:39 +1100

    Creating Tag v0.9.0

---
+++ src/script.js
@@ -0,0 +1,20 @@
+//  Lesson: Introduction to the Application Core                    
+//  Task:   Create a new application                                
+//  Task:   Add the meta data to the application         
+
+//  Create a new Application
+const app = new App({
+    metaData : {
+        name: 'Application Name',
+        version: '0.0.1',
+        description: 'Description of the application',
+        developer: 'Developer Name',
+        company: 'Company Name',
+    },
+    backgroundColor : '#000000',
+    debug: true
+});
+
+
+// Initialize application
+app.init();
